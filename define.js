6aaa1633959a79e9a939079bf8644262e2ba565c
Nis <jalipa7u7@gmail.com>
2023-03-20 18:45:54 -0600

    pepino 5 (añadiendo más proyectos)

--- define.js
+++ define.js
@@ -6,13 +6,31 @@ this.projects = [
         name:"WaBot",
         description: "Un bot para WhatsApp",
         img: "wabot-reles",
-        git: "https://github.com/MrNiz/WaBot"
+        git: "https://mrniz.github.io/blog/?d=wabot"
     },
     {
         name:"VaporeonBot",
         description: "Un bot para Discord usado para un proyecto en broma.",
         img: "vaporeon-pfp",
-        git: "https://github.com/MrNiz/VaporeBot"
+        git: "https://mrniz.github.io/blog/?d=vaporeon-bot"
+    },
+     {
+        name:"MeguminBot",
+        description: "Un bot para Discord usadando Cleverbot.",
+        img: "megumin-pfp",
+        git: "https://mrniz.github.io/blog/?d=megumin-bot"
+    },
+    {
+        name:"MrNiz page",
+        description: "Una página que actúa como mi portafolio.",
+        img: "niz-pfp",
+        git: "https://mrniz.github.io/blog/?d=mrniz-page"
+    },
+      {
+        name:"MizBlogs",
+        description: "Blogs hechos por mi para poder compartirlos hacia varias personas.",
+        img: "miz-pfp",
+        git: "https://mrniz.github.io/blog/?d=about"
     }
 ]
 
