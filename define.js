b2ef733410554f13e3c6c6397614a0731d973f0d
MrNiz <76523353+mrniz@users.noreply.github.com>
2023-07-08 19:54:23 -0600

    :v, themes and cool shit ig

--- define.js
+++ define.js
@@ -3,39 +3,77 @@
  */
 this.projects = [
     {
+        name:"(MI PRIMER JUEGO XD) Shrek",
+        description: "Shrek is love",
+        largeDesc: "Es el mejor juego de itch.io y nadie me lo puede negar creo",
+        color: "rgba(50,255,50,0.8)",
+        img: "shrek",
+        allowSs: true,
+        git: "https://mrniz.itch.io/shrek",
+        refImg: "shrekproj",
+    },
+    {
         name:"WaBot",
         description: "Un bot para WhatsApp",
+        largeDesc: "Un template más bien dicho de un bot para WhatsApp, actualmente está descontinuado el desarrollo, y dudo continuarlo xd, pero hey, ¿Quedó bien?",
+        color: "rgba(0,255,0,0.8)",
         img: "wabot-reles",
-        git: "https://mrniz.github.io/blog/?d=wabot"
+        git: "https://mrniz.github.io/blog/?d=wabot",
+        refImg: "https://mrniz.github.io/blog/img/wabot/using-2.jpg",
     },
     {
         name:"VaporeonBot",
         description: "Un bot para Discord usado para un proyecto en broma.",
+        largeDesc: "NO LO DIGAN.",
         img: "vaporeon-pfp",
-        git: "https://mrniz.github.io/blog/?d=vaporeon-bot"
+        color: "rgba(150,100,200,0.8)",
+        git: "https://mrniz.github.io/blog/?d=vaporeon-bot",
+        refImg: "https://mrniz.github.io/blog/img/vaporeon-bot/for.png",
     },
      {
-        name:"MeguminBot",
-        description: "Un bot para Discord usando Cleverbot (Gracias Yulie por ver este error).",
+        name:"MeguminBot - (EN MANTENIMIENTO)",
+        description: "Un bot para Discord usando Cleverbot.",
         img: "megumin-pfp",
-        git: "https://mrniz.github.io/blog/?d=megumin-bot"
+        largeDesc: "Bot demasiado simple que está en mantenimiento ya que me da webita arreglarlo xd, pero prometo regresarlo a la vida (cuando consiga una AI API barata o gratuita y conveniente).",
+        color: "rgba(255,0,0,0.8)",
+        git: "https://mrniz.github.io/blog/?d=megumin-bot",
+        refImg: "https://mrniz.github.io/blog/img/megumin-bot/banner.png",
+        miniScript: "<h1>Gracias a Yulie por corregirme:</h1><br></br> <img class='refimg' src='https://media.discordapp.net/attachments/967669387584733224/1108448144057958492/image.png'></img>",
     },
     {
         name:"MrNiz page",
         description: "Una página que actúa como mi portafolio.",
+        largeDesc: "OSEA EN PLAN HOLY SHIT",
         img: "niz-pfp",
+        color: "rgb(200,150,150)",
         git: "https://mrniz.github.io/blog/?d=mrniz-page"
     },
       {
         name:"MizBlogs",
         description: "Blogs hechos por mi para poder compartirlos hacia varias personas.",
         img: "miz-pfp",
+        largeDesc: "blogs tremendo para documentar cosas creo, no sé, ta rara la página le falta update xd.",
+        color: "gray",
         git: "https://mrniz.github.io/blog/?d=about"
     }
 ]
-
+this.splashDescription = [
+    "Persona que desarrolla juegos - fuentes: creánme",
+    "Sé multiplicar hasta 2.",
+    "JAJAJAJ TA BIEN WEI, EL OTRO DÍA LE DIJE QUE, Y DIJO SO JAJAJAJ.",
+    "Sabían que llevo desde 2016 (Más o menos) jugando Clash Royale, y aún no sé jugar?",
+    "Yo siento que Minecraft 1.25 va ser la Update más rara del mundo, recuerden mis palabras.",
+    "Me acuerdo que cuando estaba haciendo esta página dije: \"Hay que hacerla simple\"<p>¿Cómo le explicamos? ",
+    "Sé programar en Haxe, curiosamente, por FNF, Qué loco, ¿No?",
+    "Jah, sé que esta página le meto esfuerzo y no tendré ni para pagar un hosting que no sea GitHub",
+    "Además de programar, sé tocar instrumentos, ¿Por qué aparece esto en la splash desc?<p>Ni idea loco apenas programo esto, mejor ve a <b><a href=\"#about\">Sobre mí</a></b> para más información.",
+    "Cada proyecto que inicio, probablemente si es que nadie más le gusta, lo termino dejando en carpetas donde sé que nadie lo verá.",
+    "Sé programar",
+    "Agregaremos a Tilín a esta página algún día.",
+    "Por nada del mundo agregues al link twodotsv.html."
+]
 /**
- * Uh, oh blogs??
+ * 
  */
 this.blogs = [
     {
--- script.js
+++ script.js
@@ -1,25 +1,97 @@
-const projects =document.getElementsByTagName("main")[0].getElementsByTagName("section").portfolio.getElementsByTagName("ul")[0]
-const logo = document.getElementsByTagName("header")[0].getElementsByTagName("img")[0]
-console.log(projects)
-for (let project of this.projects)
-{
-    let div = document.createElement("li")
-    console.log(project)
-    div.classList.add("img")
-    div.innerHTML = `
-        <img src="img/proj/${project.img}.png"> </img> <a href="${project.git}">${project.name}</a>
-        <desc>- ${project.description}</desc>
-    `
-    console.log(div)
-    projects.appendChild(div)
+const projects = document.querySelector("main section#portfolio ul");
+const logo = document.querySelector("header img.funkin_papu_imagen_principal");
+const body = document.querySelector("body");
+
+body.classList.add("dark-mode");
+var darkMode = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+const themeButton = document.getElementById('theme-button');
+const themeIcon = document.getElementById('theme-icon');
+const themeText = document.getElementById('theme-text');
+
+themeButton.addEventListener('click', toggleTheme);
+
+function toggleTheme() {
+  darkMode = !darkMode;
+  update();
 }
+
+update();
+var curMode = "m";
+
+function update() {
+  if (curMode !== (darkMode ? "a" : "b")) {
+    themeText.src = `img/when-${!darkMode}.png`;
+    themeIcon.src = darkMode ? 'img/light-theme.png' : 'img/dark-theme.png';
+    for (let d of document.getElementsByTagName("*")) {
+      d.classList.remove("dark-mode", "white-mode");
+      d.classList.add(darkMode ? "dark-mode" : "white-mode");
+    }
+    curMode = darkMode ? "a" : "b";
+  }
+  setTimeout(update, 0.1);
+}
+
+for (let project of projects.children) {
+  let div = document.createElement("li");
+  div.classList.add("img");
+  div.innerHTML = `
+    <b style="background-color: ${project.dataset.color};">
+      <img src="img/proj/${project.dataset.img}.png">
+      <a href="${project.dataset.git}">${project.dataset.name}</a>
+      <desc>- ${project.dataset.description}</desc>
+      <p></p>
+      <descl>${project.dataset.largeDesc}</descl>
+      <center style="background-color: ${project.dataset.color};">
+        <p></p>
+        ${project.dataset.refImg ? `<img class="refimg" src="${project.dataset.allowSs ? 'img/ss/' + project.dataset.refImg +'.png' : project.dataset.refImg }">` : ""}
+        <p></p>
+        ${project.dataset.miniScript ? project.dataset.miniScript : ""}
+      </center>
+    </b>
+  `;
+  projects.appendChild(div);
+}
+
+document.querySelector("header h2").innerHTML = splashDescription[Math.floor(Math.random() * splashDescription.length)];
+
+var oldSrc = "";
+var clicked = false;
+var clicks = [0, 0];
+logo.onclick = () => {
+  console.log({ currentDay: new Date().getUTCDay(), clicks, clicked });
+  for (let i = 0; i < clicks.length; i++) {
+    clicks[i] += 1;
+  }
+  if (clicked === true) return console.log("Is already clicked!!!");
+
+  if (clicks[1] >= new Date().getUTCDay()) {
+    clicked = true;
+    logo.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/SDLG_logo.svg/1200px-SDLG_logo.svg.png";
+    setTimeout(() => {
+      logo.src = oldSrc;
+      clicked = false;
+      clicks[1] = 0;
+    }, 2500);
+    return;
+  }
+
+  if (clicks[0] >= 5) {
+    clicked = true;
+    logo.src = "https://media.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif";
+    setTimeout(() => {
+      logo.src = oldSrc;
+      clicked = false;
+      clicks[0] = 0;
+    }, 2500);
+  }
+};
 
 fetch(`https://api.github.com/users/MrNiz`)
   .then(response => response.json())
   .then(data => {
     const profilePicUrl = data.avatar_url;
-    // no sabe programar golpeenlo
     logo.src = profilePicUrl;
-    console.log(profilePicUrl);
+    oldSrc = profilePicUrl;
   })
   .catch(error => console.error(error));
